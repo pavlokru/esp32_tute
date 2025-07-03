c058ef4a08c98a4c6863c68816c1df55b8577bca
pavlokruz <pavlokruz@gmail.com>
2025-07-03 13:22:40 -0300

    iniciando archivos del proyuecto esp32 de platformIO

---
+++ src/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h> 
+void app_main() {
+
+    printf("Hello, World!\n");
+    // Add your application logic here
+    // For example, you can call other functions or perform operations
+    // that are part of your application.
+}
