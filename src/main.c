95a31ff341efbcad160a87cebe8bef1255f2c63e
pavlokruz <pavlokruz@gmail.com>
2025-07-03 15:38:35 -0300

    tercer  comentario

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 void app_main() {
 //primer comentario
 //segundo comentario
+//tercer ciomentario
     printf("Hello, World!\n");
     // Add your application logic here
     // For example, you can call other functions or perform operations
